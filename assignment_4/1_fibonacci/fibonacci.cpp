8ffa59b064d44c72a692d1552e73f2e5caebf56e
Muhammad Santriaji <santriaji@gmail.com>
2024-09-16 19:02:26 +0700

    add test to fibonacci

--- assignment_4/1_fibonacci/fibonacci.cpp
+++ assignment_4/1_fibonacci/fibonacci.cpp
@@ -1,35 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-void printFibonacci(int n)
-{
+// Function to generate the Fibonacci sequence as a string
+std::string fibonacci(int n) {
+    std::string result;
     int a = 0, b = 1, next;
 
-    std::cout << "Fibonacci sequence up to " << n << ": ";
-
-    // Print Fibonacci sequence
-    for (int i = 1; i <= n; ++i)
-    {
-        std::cout << a << " ";
-
-        // Calculate next Fibonacci number
+    for (int i = 1; i <= n; ++i) {
+        result += std::to_string(a) + " ";
         next = a + b;
         a = b;
         b = next;
     }
 
-    std::cout << std::endl;
+    return result;
 }
 
-int main()
-{
-    int n;
+int main(int argc, char* argv[]) {
+    if (argc == 1) { // Run mode with user input
+        int n;
+        std::cout << "Enter the value of n: ";
+        std::cin >> n;
 
-    // Get the user input
-    std::cout << "Enter the value of n: ";
-    std::cin >> n;
+        std::string result = fibonacci(n);
+        std::cout << "Fibonacci sequence up to " << n << ": " << result << std::endl;
 
-    // Output the Fibonacci sequence up to n
-    printFibonacci(n);
+    } else if (argc == 2 && std::string(argv[1]) == "test") { // Test mode
+        std::ifstream testFile("test.txt");
+        int input;
+        std::string expected_output, output;
+        int passed_tests = 0, test_num = 1;
+
+        if (!testFile) {
+            std::cerr << "Error: Could not open test.txt" << std::endl;
+            return 1;
+        }
+
+        while (testFile >> input && std::getline(testFile, expected_output)) {
+            // Remove the leading whitespace from expected_output
+            if (!expected_output.empty() && expected_output[0] == ' ') {
+                expected_output = expected_output.substr(1);
+            }
+
+            output = fibonacci(input);
+
+            if (output == expected_output + " ") {
+                std::cout << "Test " << test_num << " passed!" << std::endl;
+                ++passed_tests;
+            } else {
+                std::cout << "Test " << test_num << " failed. Expected: \"" 
+                          << expected_output << "\", Got: \"" << output << "\"" << std::endl;
+            }
+            ++test_num;
+        }
+
+        std::cout << "Passed " << passed_tests << " out of 10 tests." << std::endl;
+        testFile.close();
+    } else {
+        std::cerr << "Invalid arguments." << std::endl;
+    }
 
     return 0;
 }
